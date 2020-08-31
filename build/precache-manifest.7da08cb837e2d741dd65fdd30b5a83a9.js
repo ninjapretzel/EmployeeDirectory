a0cbe87eb9d95ee9daf58985df8266f190cb7ed1
ninjapretzel <ninjapretzel@yahoo.com>
2020-08-31 14:32:21 -0600

    Adds  build folder for deployment

---
+++ build/precache-manifest.7da08cb837e2d741dd65fdd30b5a83a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e9bb07cf533d534bd8fb79f8f81882",
+    "url": "/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "4153f4807961244e63cb",
+    "url": "/EmployeeDirectory/static/css/main.23e520fb.chunk.css"
+  },
+  {
+    "revision": "040d8dbfe0d6faf60309",
+    "url": "/EmployeeDirectory/static/js/2.60e8edcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmployeeDirectory/static/js/2.60e8edcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4153f4807961244e63cb",
+    "url": "/EmployeeDirectory/static/js/main.835c3aab.chunk.js"
+  },
+  {
+    "revision": "9da1e905168c93dd0a4f",
+    "url": "/EmployeeDirectory/static/js/runtime-main.f963c79a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EmployeeDirectory/static/media/logo.5d5d9eef.svg"
+  }
+]);
